886c7a6b13624066cd65bfd2b60e7356557b3b6d
guilhermehdev <guilhontina@hotmail.com>
2021-04-14 16:35:46 -0300

    order model

---
+++ app/Models/OrderItem.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model');
+
+class OrderItem extends Model {
+  static boot() {
+    super.boot();
+
+    this.addHook('beforeSave', 'OrderItemHook.updateSubtotal');
+  }
+
+  product() {
+    return this.belongsTo('App/Models/Product');
+  }
+
+  order() {
+    return this.belongsTo('App/Models/Order');
+  }
+
+  static get createdAtColumn() {
+    return null;
+  }
+
+  static get updatedAtColumn() {
+    return null;
+  }
+}
+
+module.exports = OrderItem;
