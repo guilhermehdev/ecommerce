83bfaa9b6ec23c45c13756b9e200013590d0e5e5
guilhermehdev <guilhontina@hotmail.com>
2021-04-14 11:09:39 -0300

    product model

---
+++ app/Models/Product.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model');
+
+class Product extends Model {
+  image() {
+    return this.belongsTo('App/Models/Image');
+  }
+
+  /**
+   * Relacionamento entre o produto e as imagens
+   */
+  images() {
+    return this.belongsToMany('App/Models/Image');
+  }
+
+  /**
+   * Relacionamento entre Produto e Categorias
+   */
+  categories() {
+    return this.belongsToMany('App/Models/Category');
+  }
+
+  /**
+   * Relacionamento entre produtos e cupons de descontos
+   */
+  coupons() {
+    return this.belongsToMany('App/Models/Coupon');
+  }
+}
+
+module.exports = Product;
