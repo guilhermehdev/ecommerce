5da92d7a52e964c551f4ec8f66bb1bcf38cdb2cc
guilhermehdev <guilhontina@hotmail.com>
2021-04-22 12:44:49 -0300

    Added str_random from Helpers

--- app/Models/PasswordReset.js
+++ app/Models/PasswordReset.js
@@ -2,18 +2,21 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model');
+const { str_random } = use('App/Helpers');
 
 class PasswordReset extends Model {
   static boot() {
-    super.boot;
+    super.boot();
+
     this.addHook('beforeCreate', async model => {
       model.token = await str_random(25);
-      const expires_at = new Date();
-      expires_at.setMinutes(expires_at.getMinutes() + 30);
-      model.expires_at = expires_at;
+      const now = new Date();
+      now.setMinutes(now.getMinutes() + 30);
+      model.expires_at = now;
     });
   }
 
+  // Define database date fiedls
   static get dates() {
     return ['created_at', 'updated_at', 'expires_at'];
   }
